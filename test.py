a3753baa4a9eabe5ea34f8a6ca3db5b592387740
jnadach <119163797+jnadach@users.noreply.github.com>
2022-11-27 12:45:29 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+print("W systemie ósemkowy liczba 777 reprezentuje liczbę dziesiętną: ", end='')
+print(7 * 8 ** 0 + 7 * 8 ** 1 + 7 * 8 ** 2)
+# print(0o777)
+
+print("W systemie binarnym liczba 1011 reprezentuje liczbę dziesiętną: ", end='')
+print(1 * 2 ** 0 + 1 * 2 ** 1 + 0 * 1 ** 2 + 1 * 2 ** 3)
+# print(0b1011)
+
+print("W systemie szesnastkowym liczba FFF reprezentuje liczbę dziesiętną: ", end='')
+print(15 * 16 ** 0 + 15 * 16 ** 1 + 15 * 16 ** 2)
+# print(0xFFF)
+
+print("W systemie piątkowym liczba 123 reprezentuje liczbę dziesiętną: ", end='')
+print(3 * 5 ** 0 + 2 * 5 ** 1 + 1 * 5 ** 2)
+# 123
